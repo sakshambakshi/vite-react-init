840b1642f017dce2f5bcc1d5b2482e74a5ab18f1
Your Name <you@example.com>
2023-10-04 23:12:55 +0530

    installintion

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: 'src', replacement: '/src' }],
-   }
+    alias: [
+      { find: 'src', replacement: '/src' },
+      { find: 'store', replacement: '/src/store' },
+    ],
+  }
 })
